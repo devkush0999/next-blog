0f95949e6ec8d736706c96c2bb34334feeabf157
devkush0999 <devesh2cse@gmail.com>
2024-12-01 22:24:04 +0530

    .

--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs';
 
 export default function SignUpPage() {
   return (
-    <div className='flex items-center justify-center p-3'>
+    <div className='flex items-center justify-center p-4'>
       <SignUp />
     </div>
   );
